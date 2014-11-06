1d100fc997e83586f49152dd077c0236fadc5274
Max Edwards <maxedwar@thoughtworks.com>
2014-11-06 21:34:41 +0000

    updated superuser script so that it can work from blank vm

--- ansible/roles/monitoring/sentry/templates/superuser.py
+++ ansible/roles/monitoring/sentry/templates/superuser.py
@@ -4,5 +4,8 @@ configure()
 from django.db import DEFAULT_DB_ALIAS as database
 from sentry.models import User
 
-if User.objects.get(username__exact='{{ superuser_sentry.username }}') is None:
-    User.objects.db_manager(database).create_superuser('{{ superuser_sentry.username }}', '{{ superuser_sentry.email }}', '{{ superuser_sentry.password }}')
+try:
+	if User.objects.get(username__exact='{{ superuser_sentry.username }}') is None:
+		User.objects.db_manager(database).create_superuser('{{ superuser_sentry.username }}', '{{ superuser_sentry.email }}', '{{ superuser_sentry.password }}')
+except:
+	User.objects.db_manager(database).create_superuser('{{ superuser_sentry.username }}', '{{ superuser_sentry.email }}', '{{ superuser_sentry.password }}')
